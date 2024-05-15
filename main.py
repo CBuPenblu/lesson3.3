7e3b1d73e8a430c3d7490563a70dd0b5095ca946
Igors <166141617+CBuPenblu@users.noreply.github.com>
2024-05-16 00:16:07 +0300

    add all const

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
+import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 768
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Alien Shooter")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/istockphoto-1327876790-612x612.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 100
+target_height = 100
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
